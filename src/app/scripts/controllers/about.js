d597899cba5c21c30cae083c4598ac19506fe9cf
Tay Yang Shun <tay.yang.shun@gmail.com>
2016-04-25 22:13:00 +0800

    Add angular skeleton

---
+++ src/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dayspringApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dayspringApp
+ */
+angular.module('dayspringApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ src/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dayspringApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dayspringApp
+ */
+angular.module('dayspringApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
