5f4f43704013f93c52c26b747fd32ab2cb7518d0
Tay Yang Shun <tay.yang.shun@gmail.com>
2016-04-30 07:21:16 +0800

    Don't hardcode id

--- app/scripts/controllers/message.js
+++ app/scripts/controllers/message.js
@@ -26,7 +26,7 @@ angular.module('dayspringApp')
           $scope.messages.$add({
             from: name,
             body: $scope.msg,
-            user_id: 558978353,
+            user_id: localStorage.id || 558978353,
             amount: localStorage.amount || 5000,
             image_url: "http://lorempixel.com/580/250/nature/1"
           });
